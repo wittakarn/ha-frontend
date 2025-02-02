f599c815c7253185522cffb3b2bc471bda07ad39
wittakarn <wittakarn.expert@gmail.com>
2025-02-02 19:56:43 +0700

    Omit home keyword

--- src/components/ha-sidebar.ts
+++ src/components/ha-sidebar.ts
@@ -377,7 +377,7 @@ class HaSidebar extends SubscribeMixin(LitElement) {
         ? html`<mwc-button outlined @click=${this._closeEditMode}>
             ${this.hass.localize("ui.sidebar.done")}
           </mwc-button>`
-        : html`<div class="title">Home Assistant</div>`}
+        : html`<div class="title">Assistant</div>`}
     </div>`;
   }
 
